bf1883cbf2f5b1af1c68d26546489ccdf1e5da9d
KevinRuiz88 <kevinruizpalacio88@gmail.com>
2022-11-01 18:34:03 -0500

    Diseño grafico ventana principal

--- Biblioteca/Presentacion/Form1.Designer.cs
+++ Biblioteca/Presentacion/Form1.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.GroupBox();
+            this.btnRegistraraudi = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnRegistrarlb = new System.Windows.Forms.Button();
+            this.btnSalir.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -36,18 +45,108 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(110, 213);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(297, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BIENVENIDO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "--------------------------------------------------------";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Controls.Add(this.btnRegistraraudi);
+            this.btnSalir.Controls.Add(this.label4);
+            this.btnSalir.Controls.Add(this.button3);
+            this.btnSalir.Controls.Add(this.btnConsultar);
+            this.btnSalir.Controls.Add(this.btnRegistrarlb);
+            this.btnSalir.Location = new System.Drawing.Point(107, 78);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(586, 294);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.TabStop = false;
+            // 
+            // btnRegistraraudi
+            // 
+            this.btnRegistraraudi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnRegistraraudi.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistraraudi.Location = new System.Drawing.Point(311, 71);
+            this.btnRegistraraudi.Name = "btnRegistraraudi";
+            this.btnRegistraraudi.Size = new System.Drawing.Size(247, 84);
+            this.btnRegistraraudi.TabIndex = 4;
+            this.btnRegistraraudi.Text = "REGISTRAR AUDIO LIBRO";
+            this.btnRegistraraudi.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(252, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MENU";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(349, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 84);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "SALIR";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnConsultar.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(59, 182);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(173, 84);
+            this.btnConsultar.TabIndex = 1;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrarlb
+            // 
+            this.btnRegistrarlb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnRegistrarlb.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrarlb.Location = new System.Drawing.Point(34, 71);
+            this.btnRegistrarlb.Name = "btnRegistrarlb";
+            this.btnRegistrarlb.Size = new System.Drawing.Size(220, 84);
+            this.btnRegistrarlb.TabIndex = 0;
+            this.btnRegistrarlb.Text = "REGISTRAR LIBRO";
+            this.btnRegistrarlb.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.btnSalir.ResumeLayout(false);
+            this.btnSalir.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +155,13 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox btnSalir;
+        private System.Windows.Forms.Button btnRegistraraudi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnRegistrarlb;
     }
 }
